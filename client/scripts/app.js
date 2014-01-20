0b4b2d70b27be7f197d1575b2fb83e8c8786d5fb
Hack Reactor Students <alexmckenley@gmail.com>
2014-01-20 15:11:46 -0800

    implemented date comparison

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,29 @@
-$(document).ready(function() {
-  var url = 'https://api.parse.com/1/classes/chatterbox';
+var latest = 0;
+var url = 'https://api.parse.com/1/classes/chatterbox';
 
-  $.get(url, function(response) {
-    var messages = response.results;
-    for (var i = 0; i < messages.length; i++) {
-      var node = $("<div class='chat'></div>");
-      var temp = $("<p></p>");
-      var user = temp.text(messages[i].username);
-      var message = temp.text(messages[i].text);
-      var time = temp.text(messages[i].createdAt);
-      node.append(user, message, time);
-      $(".chats").append(node);
-    }
-  });
+  var getChats = function() {
+    $.get(url + "?order=-createdAt", function(response) {
+      var messages = response.results;
+      for (var i = messages.length -1; i >= 0 ; i--) {
+        var date = new Date(messages[i].createdAt);
+        if (date > latest) {
+          var node = $("<div class='chat'></div>");
+          var user = $("<p></p>").text(messages[i].username);
+          var message = $("<p></p>").text(messages[i].text);
+          var time = $("<p></p>").text(messages[i].createdAt);
+          node.append(user, message, time);
+          $(".chats").prepend(node);
+        }
+        if (i === 0) {
+          latest = messages[i].createdAt;
+        }
+      }
+    });
+  };
+
+$(document).ready(function() {
+
+  getChats();
+  setInterval(getChats, 1000);
 
 });
