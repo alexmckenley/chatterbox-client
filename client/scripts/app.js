b29e1fa814ae0c23484149e8d3c79a1dee00d564
Hack Reactor Students <alexmckenley@gmail.com>
2014-01-20 14:49:49 -0800

    Getting chats

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,17 @@
-// YOUR CODE HERE:
+$(document).ready(function() {
+  var url = 'https://api.parse.com/1/classes/chatterbox';
+
+  $.get(url, function(response) {
+    var messages = response.results;
+    for (var i = 0; i < messages.length; i++) {
+      var node = $("<div class='chat'></div>");
+      var temp = $("<p></p>");
+      var user = temp.text(messages[i].username);
+      var message = temp.text(messages[i].text);
+      var time = temp.text(messages[i].createdAt);
+      node.append(user, message, time);
+      $(".chats").append(node);
+    }
+  });
+
+});
